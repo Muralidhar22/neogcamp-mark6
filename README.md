1bcc4d88c16af0233b781f56f80da4dffb1869bb
Muralidhar <muralidhar7794@gmail.com>
2022-06-13 12:47:37 +0530

    fix: style added, html structure change

---
+++ README.md
@@ -0,0 +1,9 @@
+# Minion Translator Language
+
+## About
+This is Minion translator app, [Fun Translation](https://funtranslations.com/api/) API is used to convert the English text entered into the Banana Language
+
+## Languages
+- HTML, CSS, Javascript
+- [Fetch(API)](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch)
+
--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
+const btnTranslate = document.querySelector("#btn-translate");
+const txtInput = document.querySelector("#txt-input");
+const outputDiv = document.querySelector("#output");
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
+const serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(input) {
-    return `${serverURL}?text${input}`
+    return `${serverURL}?text=${input}`
 }
 
 function errorHandler(error) {
@@ -18,13 +17,13 @@ function errorHandler(error) {
 
 
 function clickHandler() {
-    var inputText = txtInput.value; // taking input
+    const inputText = txtInput.value; // taking input
 
     // calling server for processing
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
-            var translatedText = json.contents.translated;
+            const translatedText = json.contents.translated;
             outputDiv.innerText = translatedText; // output
            })
         .catch(errorHandler)
